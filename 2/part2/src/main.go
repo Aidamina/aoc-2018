fde74139254a11498bba2804b7481244f08e0a9b
Tjeerd Jan van der Molen <tjeerdjan.vandermolen@meteogroup.com>
2018-12-03 16:01:50 +0100

    cleanup

--- 2/part2/src/main.go
+++ 2/part2/src/main.go
@@ -14,13 +14,11 @@ func main() {
 		id := scanner.Text()
 		ids = append(ids, id)
 	}
-
 	size := len(ids[0])
 	for i := range make([]int, size) {
 		var seen = map[string]bool{}
 		for _, id := range ids {
 			fid := id[0:i] + id[i+1:size]
-			fmt.Println(id + " => " + fid)
 			if _, ok := seen[fid]; ok {
 				fmt.Println(fid)
 				os.Exit(0)
@@ -28,4 +26,5 @@ func main() {
 			seen[fid] = true
 		}
 	}
+	os.Exit(1)
 }
