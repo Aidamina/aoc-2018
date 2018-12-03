0ba06b4f9690c6f54285a746fef81e2bf2aff702
Tjeerd Jan van der Molen <tjeerdjan.vandermolen@meteogroup.com>
2018-12-03 11:36:00 +0100

    part2

--- 1/part2/src/main.go
+++ 1/part2/src/main.go
@@ -10,12 +10,18 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(reader)
+	var reached = map[int]bool{0: true}
 	var frequency int
 	for scanner.Scan() {
 		delta, err := strconv.Atoi(scanner.Text())
 		if err == nil {
 			frequency += delta
+			if _, ok := reached[frequency]; ok {
+				fmt.Println(frequency)
+				os.Exit(0)
+			}
+			reached[frequency] = true
 		}
 	}
-	fmt.Println(frequency)
+	os.Exit(1)
 }
